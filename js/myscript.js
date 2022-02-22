64271a1fbc60954db79f9604c5d3e7cb5f7e8c2d
Donatella7 <campaniellodonatella@gmail.com>
2022-02-22 15:02:52 +0100

    input user

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+const kmUser = prompt("Quanti km vuoi fare?");
+const ageUser = prompt("Quanti anni hai?");
+
+document.getElementById("my-km").innerHTML = kmUser;
+document.getElementById("my-age").innerHTML = ageUser;
