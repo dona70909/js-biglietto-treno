4cb117f5ae35dec0cc1f1143d48dec9309f49514
Donatella7 <campaniellodonatella@gmail.com>
2022-02-22 15:29:11 +0100

    convalida km

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,16 @@
 /* User info const */
-const kmUser = prompt("Quanti km vuoi fare?");
-const ageUser = prompt("Quanti anni hai?");
+let kmUser = parseFloat(prompt("Quanti km vuoi fare?"));
+const ageUser =parseFloat(prompt("Quanti anni hai?"));
 
 /* display User info inside the html */
 document.getElementById("my-km").innerHTML = kmUser;
 document.getElementById("my-age").innerHTML = ageUser;
 
 /* convalida dei dati */
+/* !convalida km */
+if(isNaN(kmUser)){
+   kmUser = Math.floor(Math.random() * 100) + 1; 
+    document.getElementById("my-km").innerHTML = ("km inseriti dal pc ") +  kmUser;
+    
+} 
+
