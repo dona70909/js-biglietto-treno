5a95a9523d3a15639c25f27ce2d878038f368b74
Donatella7 <campaniellodonatella@gmail.com>
2022-02-22 16:48:10 +0100

    fine

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,6 @@ let ageUser =parseFloat(prompt("Quanti anni hai?"));
 let calcoloPrice = 0;
 const kmPrice = 0.21;
 const adultAge = 18;
-const discountUnder = 0.2;
 const elderlyAge = 65;
 
 
@@ -12,22 +11,40 @@ const elderlyAge = 65;
 document.getElementById("my-km").innerHTML = kmUser + (" km");
 document.getElementById("my-age").innerHTML = ageUser + (" anni");
 
+
+
+/* CONVALIDA E RANDOM VALUE   */
+
 /* convalida dei dati : dati creati random se non sono giusti or non calcolo il prezzo direttamente  */
 /* !convalida km */
-if(isNaN(kmUser)){
+/* if(isNaN(kmUser)){
     kmUser = Math.floor(Math.random() * 100) + 1; 
     document.getElementById("my-km").innerHTML = ("km inseriti dal pc ") +  kmUser + (" km");
 } 
 
 /* !convalida age */
-if(isNaN(ageUser)){
+/* if(isNaN(ageUser)){
     ageUser = Math.floor(Math.random() * 10) + 1; 
     document.getElementById("my-age").innerHTML = ageUser +(" anni");
-}
+} */ 
 
 
+/* if (ageUser <= adultAge){
+    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100)).toFixed(2);
+    document.getElementById("my-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
+} else if(ageUser > elderlyAge){
+    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100)).toFixed(2);
+    document.getElementById("my-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
+} else{
+    calcoloPrice = (kmUser * kmPrice).toFixed(2);
+    document.getElementById("my-price").innerHTML = calcoloPrice + (" euro");
+} */
 
-if (ageUser <= adultAge){
+/* CONVALIDA E NON CALCOLA IL PREZZO */
+
+if (isNaN(kmUser) || isNaN(ageUser)){
+    document.getElementById("my-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
+} else if (ageUser <= adultAge){
     calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100)).toFixed(2);
     document.getElementById("my-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
 } else if(ageUser > elderlyAge){
@@ -37,3 +54,6 @@ if (ageUser <= adultAge){
     calcoloPrice = (kmUser * kmPrice).toFixed(2);
     document.getElementById("my-price").innerHTML = calcoloPrice + (" euro");
 }
+
+
+
