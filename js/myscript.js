0423acf1d3e2a7c6c3dab4c605fbc6f21c56117d
Donatella7 <campaniellodonatella@gmail.com>
2022-02-23 09:47:00 +0100

    comment

--- js/myscript.js
+++ js/myscript.js
@@ -15,11 +15,11 @@ document.getElementById("my-age").innerHTML = ageUser + (" anni");
 /* CONVALIDA E NON CALCOLA IL PREZZO */
 if ( (isNaN(kmUser) || isNaN(ageUser)) || ((kmUser < 0) || (kmUser > 1000)) || ((ageUser < 0) ||(ageUser > 130)) ){
     document.getElementById("my-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
-} else if (ageUser <= adultAge){
-    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100)).toFixed(2);
+} else if (ageUser < adultAge){
+    calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
     document.getElementById("my-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
 } else if(ageUser > elderlyAge){
-    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100)).toFixed(2);
+    calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
     document.getElementById("my-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
 } else{
     calcoloPrice = (kmUser * kmPrice).toFixed(2);
